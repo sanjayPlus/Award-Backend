8d99ec8bd5dd453bf228bc7241ce338bd36df71a
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-02-14 14:40:27 +0530

    sorting chnaged

--- controllers/userController.js
+++ controllers/userController.js
@@ -393,8 +393,8 @@ const bloodDonation2 = async (req, res) => {
         }
         
         if (place) {
-            filterData = filterData?.filter(item => item?.place.toLowerCase()?.includes(place?.toLowerCase()))
-        }
+            filterData = filterData.filter(item => item?.place?.toLowerCase().includes(place.toLowerCase()));
+        }        
         
         if (blood_group) {
             filterData = filterData.filter((user) => user.blood_group === blood_group);
